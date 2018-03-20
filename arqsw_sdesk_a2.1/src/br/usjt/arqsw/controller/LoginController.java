ba22274987496abd4f9209053f1603e037b23476
thg.barbosa28 <thg.barbosa28@gmail.com>
2018-03-19 21:31:06 -0300

    aula

---
+++ arqsw_sdesk_a2.1/src/br/usjt/arqsw/controller/LoginController.java
@@ -0,0 +1,59 @@
+package br.usjt.arqsw.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.usjt.arqsw.dao.UsuarioDAO;
+import br.usjt.arqsw.entity.Usuario;
+import br.usjt.arqsw.service.UsuarioService;
+
+/**
+ * 
+ * @author Thiago Barbosa Palma 816117515
+ * 
+ *
+ */
+
+@Controller
+public class LoginController {
+	UsuarioService service;
+	
+	@Autowired
+	public LoginController(UsuarioService srv){
+		this.service = srv;
+	}
+	
+	
+	@RequestMapping("LoginForm")
+	public String loginForm(){
+		return "formulario-login";
+	}
+	
+		
+	@RequestMapping("fazer_login")
+	public String fazerLogin(Usuario usuario, HttpSession session){
+		try {
+			if(service.validarUsuario(usuario)){
+				session.setAttribute(Usuario.LOGADO, usuario);
+				return "web";
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "Erro";
+		}
+		return "redirect:loginForm";
+	}
+	
+	@RequestMapping("logout")
+	public String logout(HttpSession session){
+		session.setAttribute(Usuario.LOGADO, null);
+		return "formulario-login";
+	}
+
+
+}
